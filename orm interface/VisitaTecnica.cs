fca1117b28a9c27c55b7179ff898f8cc99190034
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:13:40 -0300

    old project migrated

---
+++ orm interface/VisitaTecnica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class VisitaTecnica : iSolicitacao
+    {
+        public int nr_solicitacao { get; set; }
+        public string dsSolicitacao { get; set; }
+        public string nmSolicitante { get; set; }
+
+
+        public void imprimir()
+        {
+        }
+    }
+}
---
+++ orm interface/iSolicitacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    interface iSolicitacao
+    {
+        int nr_solicitacao { get; set; }
+        string dsSolicitacao { get; set; }
+        string nmSolicitante { get; set; }
+
+
+        void imprimir();
+    }
+}
